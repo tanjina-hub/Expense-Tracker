a1dfd96bf8cc8d1421d3d2c9317bf559b1fe95f1
tanjina-hub <67520120+tanjina-hub@users.noreply.github.com>
2021-02-21 14:02:59 +0100

    final commit

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,17 @@
+import { Transaction } from "../components/Transaction";
+
 export default (state, action) => {
     switch(action.type) {
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(Transaction => Transaction.id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
         default:
             return state;
     }
